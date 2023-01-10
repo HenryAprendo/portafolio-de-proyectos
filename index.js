cb0dbf8de7ce99f1def3eccee8703673d8a17edc
Henry Salazar <hjsmipuc@gmail.com>
2023-01-10 12:00:30 -0500

    Maquetado y estilos a la seccion de proyectos

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ const btnCloseMenu = document.querySelector('[data-close-menu]');
 btnCloseMenu.addEventListener('click', () => {
     contentMenu.classList.remove('show__menu');
 });
+
+
+// seccion de proyectos
+
+const projectContents = document.querySelectorAll('.content');
+projectContents.forEach( item => item.classList.add('show'));
+
+const tabs = document.querySelectorAll('[data-tab]');
+tabs.forEach(tab => {
+    tab.addEventListener('click', (e) => {
+        const element = e.target;
+        const index = element.dataset.tab;
+        projectContents[index].classList.toggle('show');
+        element.classList.toggle('active');
+    });
+});
